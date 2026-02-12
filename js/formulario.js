aa586f8ee1bc1320cd1be907a96b8fd99b826ec8
Lore <lorem.lr.design@gmail.com>
2026-02-12 12:09:38 +0100

    Formulario + script + alerta con SweetAlert2 (biblioteca JS)

--- js/formulario.js
+++ js/formulario.js
@@ -19,14 +19,34 @@ document.addEventListener("DOMContentLoaded", function(){
 
     const form = document.getElementById("formAdopcion");
 
-    form.addEventListener("submit",function(event){
-        if(radioSi.checked){
-            const cantidadInput = document.querySelector("input");
-            if(cantidadInput.value===""){
-                event.preventDefault();
-                alert("Por favor, indica cuántas mascotas tienes.");
+    document.getElementById("formAdopcion").addEventListener("submit", function(){
+        event.preventDefault();
+        const datos = new FormData(form);
+
+        fetch (form.action, {
+            method: "POST",
+            body: datos,
+            headers:{
+                'Accept':'application/json'
             }
-        }
-    });
+        }).then (Response =>{
+            if(Response.ok){
+                Swal.fire({
+                    title:"Solicitud enviada",
+                    text:"Tu formulario ha sido enviado corrrectamente.",
+                    icon:"success",
+                    confirmButtonText: "Aceptar",
+                });
+                form.reset();
+                cantidadContainer.classList.add("d-none");
+            } else {
+                Swal.fire({
+                    title:"Error",
+                    text:"Hubo un problema al enviar el formulario. Vuelve a intentarlo.",
+                    icon:"error",
+                });
+            }
+        })
+    })
 
 });
